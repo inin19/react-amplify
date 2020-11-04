c7b5a9dcf081a7d8422da1ad10dcb2ad3547b3ed
Yingying Chen <cyy870119@gmail.com>
2020-11-04 13:39:43 -0500

    pushed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import AmplifyTheme from 'aws-amplify-react/dist/AmplifyTheme';
 
 class App extends React.Component {
   state = {};
@@ -9,4 +11,20 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const theme = {
+  ...AmplifyTheme,
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: 'var(--amazonOrange)',
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '5px',
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: 'var(--squidInk)'
+  }
+};
+
+export default withAuthenticator(App, true, [], null, theme);
